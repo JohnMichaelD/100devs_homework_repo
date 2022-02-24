dd126dabbb8e81b649839dc9c8466ff97b00cfae
John Darrin <john.m.darrin@gmail.com>
2022-02-23 21:39:24 -0500

    updated js practice

--- Week 7/class14-materials/simple-functions/js/main.js
+++ Week 7/class14-materials/simple-functions/js/main.js
@@ -1,5 +1,26 @@
 //create a function that adds two numbers and alerts the sum
 
+function addTwoNumbers(number1, number2) {
+    let sum = number1 + number2;
+    alert(sum);
+}
+ 
+addTwoNumbers(3, 4);
+
 //create a function that multiplys three numbers and console logs the product
 
+function multThreeNumbers(number1, number2, number3) {
+    let sum = number1 * number2 * number3;
+    console.log(sum);
+}
+
+multThreeNumbers(3, 4, 6);
+
 //create a function that divides two numbers and returns the ???
+
+function divideTwoNumbers(number1, number2) {
+    let sum = number1 / number2;
+    return sum;
+}
+
+divideNumbers(5, 12);
