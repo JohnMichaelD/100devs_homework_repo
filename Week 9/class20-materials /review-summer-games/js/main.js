64c1d801cb9deef11cb09a8992980c56d59ff4f4
John Darrin <john.m.darrin@gmail.com>
2022-03-17 22:13:00 -0400

    day 20 - arrays practice

--- Week 9/class20-materials /review-summer-games/js/main.js
+++ Week 9/class20-materials /review-summer-games/js/main.js
@@ -1,1 +1,11 @@
-//Create a function that takes in an array of numbers. Multiply each number together and alert the product. 
+//Create a function that takes in an array of numbers. 
+//Multiply each number together and alert the product. 
+
+function multNumbers(arr){
+    let product = 1;
+    /*for (let i = 0; i < arr.length; i++){
+        product *= arr[i];
+    }*/
+    arr.forEach( num => product *= num)
+    alert(product);
+}
--- Week 9/class20-materials /review-winter-games/js/main.js
+++ Week 9/class20-materials /review-winter-games/js/main.js
@@ -1,1 +1,13 @@
-//Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
+//Create a function that takes in an array of numbers. 
+//Return a new array containing every even number 
+//from the original array (do not use map or filter)
+
+function createEvenArray(arr){
+    let evens = [];
+
+    for(let i = 0; i < arr.length; i++){
+        if (arr[i] % 2 === 0){
+            evens.push(arr[i]);
+        }
+    } return evens;
+}
