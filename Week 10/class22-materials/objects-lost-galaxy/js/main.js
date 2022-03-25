78f4ce4875db513b06df341b7ef8285b513a5d7f
John Darrin <john.m.darrin@gmail.com>
2022-03-25 07:20:02 -0400

    object practice

---
+++ Week 10/class22-materials/objects-lost-galaxy/js/main.js
@@ -0,0 +1,12 @@
+//Create a mouse object that has four properties and three methods
+
+const mouse = {}
+
+    mouse.brand = "Razor"
+    mouse.scrollWheel = true
+    mouse.blueTooth = false
+    mouse.color = "black"
+
+    mouse.leftClick() = function(){
+        console.log("You clicked the left button.");
+    }
