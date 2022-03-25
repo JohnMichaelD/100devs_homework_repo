e40be86214e21a94b35bf7554fa824061cfc7889
John Darrin <john.m.darrin@gmail.com>
2022-03-25 15:01:57 -0400

    objects, constructors, .this

--- Week 10/class22-materials/objects-lost-galaxy/js/main.js
+++ Week 10/class22-materials/objects-lost-galaxy/js/main.js
@@ -2,11 +2,20 @@
 
 const mouse = {}
 
-    mouse.brand = "Razor"
-    mouse.scrollWheel = true
-    mouse.blueTooth = false
-    mouse.color = "black"
+    mouse.brand = "Razor",
+    mouse.scrollWheel = true,
+    mouse.blueTooth = false,
+    mouse.color = "black",
 
     mouse.leftClick() = function(){
         console.log("You clicked the left button.");
+    },
+    mouse.rightClick() = function(){
+        console.log("You clicked the right button.");
+    },
+    mouse.scrollUp() = function(){
+        console.log("You scrolled up!");
+    },
+    mouse.scrollDown() = function(){
+        console.log("You scrolled Down!");
     }
--- Week 10/class22-materials/objects-street-fighter/js/main.js
+++ Week 10/class22-materials/objects-street-fighter/js/main.js
@@ -1,1 +1,17 @@
-//Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+//Create a street fighter constructor that 
+//makes fighting game characters with 4 properties and 3 methods
+
+function MakeStreetFighter(fighterName, fighterPower, fighterSpecialty, fighterElement){
+    this.name = fighterName,
+    this.power = fighterPower,
+    this.specialty = fighterSpecialty,
+    this.element = fighterElement,
+
+    this.specialAbility = function(){
+        alert(`Finish them with the ${this.specialty} specialty!`);
+    }
+
+    this.elementPower = function(){
+        alert(`Finish them with the ${this.element} specialty!`);
+    }
+}
