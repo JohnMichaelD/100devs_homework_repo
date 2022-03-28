f1b1ab342a5ab82aa543d01723c9bf9863e3166d
John Darrin <john.m.darrin@gmail.com>
2022-03-28 10:30:25 -1000

    object literal notation

--- Past Classes/Week 10/class22-materials/objects-lost-galaxy/js/main.js
+++ Past Classes/Week 10/class22-materials/objects-lost-galaxy/js/main.js
@@ -1,6 +1,25 @@
 //Create a mouse object that has four properties and three methods
 
-const mouse = {}
+const mouse = {};
+
+mouse.brand = "Razor",
+mouse.wireless = true,
+mouse.scrollWheel = false,
+mouse.buttonsAmount = 6,
+
+mouse.leftClick() = function(){
+    console.log("You clicked the left button!");
+}
+
+mouse.rightClick() = function(){
+    console.log("You clicked the right button!");
+}
+
+mouse.connected() = function(){
+    console.log("You are connected!");
+}
+
+/* const mouse = {}
 
     mouse.brand = "Razor",
     mouse.scrollWheel = true,
@@ -18,4 +37,4 @@ const mouse = {}
     },
     mouse.scrollDown() = function(){
         console.log("You scrolled Down!");
-    }
+    } */
