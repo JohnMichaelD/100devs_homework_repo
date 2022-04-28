bfa225a8163d7bc27258c84663c50a51d643121a
John Darrin <john.m.darrin@gmail.com>
2022-04-28 14:06:43 -0400

    object classes

--- Week 12/class26-materials/objects-constructor-fun/js/main.js
+++ Week 12/class26-materials/objects-constructor-fun/js/main.js
@@ -19,3 +19,22 @@ function PizzaMachine(size,toppings,crust,sauce){
 }
 
 let pizza = new PizzaMachine('Large',['spinach','garlic'],'Deep Dish','Heavy')
+
+function PizzaMachineTwo(size,toppings,crust,sauce){
+    this.size = size
+    this.toppings = toppings
+    this.crust = crust
+    this.sauce = sauce
+
+    this.estimatedDeliveryTime = function(){
+        console.log('Calculating')
+    }
+
+    this.burnMouth = function(){
+        console.log('oiOIWHEPFIHEFJOis');
+    }
+
+    this.frisbee = function(){
+        console.log('YEEEET')
+    }
+}
